414c85cecc9403a6579593a46ab3177c88a91443
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-11-22 14:45:12 -0300

    Creating the NEW component and the SCROLLVIEW of newness.

--- RentAll/src/pages/Home/Home.tsx
+++ RentAll/src/pages/Home/Home.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { View, Text } from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './StyleHome';
 import New from '../../components/New';
 
 
 export default function Home() {
+    const navigation = useNavigation();
+
     return (
         <ScrollView
         showsVerticalScrollIndicator={false}
@@ -32,20 +35,20 @@ export default function Home() {
                 cover={require('../../assets/house1.jpg')}
                 name="Casa de Praia"
                 description="Casa a uma quadra do mar, lugar seguro e monitorado 24 horas"
+                onPress={() => navigation.navigate('detail')}
+                />
+
+                <New
+                cover={require('../../assets/house2.jpg')}
+                name="Rancho SP"
+                description="Local agradavel Longe de poluição com piscina e varios quartos separados"
                 onPress={() => {}}
                 />
 
                 <New
-                cover={require('../../assets/house1.jpg')}
-                name="Casa de Praia"
-                description="Casa a uma quadra do mar, lugar seguro e monitorado 24 horas"
-                onPress={() => {}}
-                />
-
-                <New
-                cover={require('../../assets/house1.jpg')}
-                name="Casa de Praia"
-                description="Casa a uma quadra do mar, lugar seguro e monitorado 24 horas"
+                cover={require('../../assets/house3.jpg')}
+                name="Casa Floripa"
+                description="Belezas naturais, variedade de praias, caracterizada por sua arquitetura colonial."
                 onPress={() => {}}
                 />
             </ScrollView>
